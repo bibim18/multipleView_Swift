02c148d701f5ad9f488f6a32fe49837ac693a9a9
Admin <admin@MAC3.local>
2019-02-12 10:55:47 +0700

    Fixed bug for addItem

--- multipleView/ViewController.swift
+++ multipleView/ViewController.swift
@@ -13,8 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "Menu"
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: Selector(("addItem")))
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(showItems))
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    @objc func showItems(){
+        print("fffff")
     }
     @IBAction func unwindToSegue(segue: UIStoryboardSegue){}
 
